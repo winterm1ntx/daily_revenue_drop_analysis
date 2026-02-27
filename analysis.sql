f2a75a2b3b2bba84f9c938ed02f967ae24bf4e65
winterm1ntx <rinaldiwalteva404@gmail.com>
2026-02-27 18:49:58 +0700

    submit

--- analysis.sql
+++ analysis.sql
@@ -7,7 +7,7 @@ count(customer_name) as customer_not_null,
 count(city) as city_not_null
 from retail_transactions;
 
-with daily_revenue as ( select date(date) as date , sum(total_items*total_cost) as revenue
+with daily_revenue as ( select date(date) as date , sum(total_cost) as revenue
 from retail_transactions
 group by 1 )
 select d.date , d.revenue as revenue_today ,
@@ -21,40 +21,88 @@ join daily_revenue as y
 on y.date = d.date - interval '1 day'
 join daily_revenue as w
 on w.date = d.date - interval '7 day'
-order by 1
+order by 1 ;
 
-with daily_summary as (
-select date , volume_order , revenue *1.0 / volume_order as aov , lag(volume_order)over(order by date) as previous_volume_order , 
-lag(revenue *1.0 / volume_order)over(order by date) as previous_aov
-from (
-select date(date) as date , sum(total_items*total_cost) as revenue , count(distinct transaction_id) as volume_order
-from retail_transactions
-group by 1 ))
-select date , volume_order , 
-round(case when previous_volume_order = 0 then null
-else ( volume_order - previous_volume_order ) *1.0 / previous_volume_order *100 end,2) as dod_growth_order,
-aov,
-round(case when previous_aov = 0 then null
-else ( aov - previous_aov ) *1.0 / previous_aov *100 end,2) as dod_growth_aov
-from daily_summary
-where date = date '2024-05-18'
 
-with customer_summary as ( select date , total_customer , 
-round(case when lag(total_customer)over(order by date) = 0 then null 
-else ( total_customer - lag(total_customer)over(order by date) ) *1.0 /lag(total_customer)over(order by date) *100 end,2) as dod_growth_customer
-from ( select date(date) as date , count(distinct customer_name) as total_customer 
+with daily_level as ( select date(date) as date , sum(total_cost) / count(distinct transaction_id) as aov , 
+count(distinct transaction_id) as volume_order
 from retail_transactions
-group by 1 ) )
+group by 1 )
+, daily_summary as ( select d.date , d.volume_order ,
+round ( case when y.volume_order = 0 then null
+else ( d.volume_order - y.volume_order ) *1.0 / y.volume_order *100 end , 2 ) as dod_growth_order ,
+d.aov ,
+round ( case when y.aov = 0 then null
+else ( d.aov - y.aov ) *1.0 / y.aov *100 end , 2 ) as dod_growth_aov
+from daily_level as d
+join daily_level as y
+on y.date = d.date - interval '1 day'
+)
 select*
-from customer_summary
-where date = date '2024-05-18'
+from daily_summary
+where date = date '2024-04-18' ;
 
-with city_summary as ( select* , round(case when lag(total_transaksi)over(partition by city order by date) = 0 then null
-else ( total_transaksi - lag(total_transaksi)over(partition by city order by date) ) *1.0 / lag(total_transaksi)over(partition by city order by date) * 100 end , 2 ) as city_dod_growth
-from (
-select date(date) as date , city , count(distinct transaction_id) as total_transaksi
+with total_customer as ( select date(date) as date , count(distinct customer_name) as total_customer
 from retail_transactions
-group by 1 , 2 ))
+group by 1 )
+, customer_growth as ( select d.date , d.total_customer , round( case when y.total_customer = 0 then null
+else ( d.total_customer - y.total_customer ) * 1.0 / y.total_customer *100 end , 2 ) as dod_growth_customer
+from total_customer as d
+join total_customer as y
+on y.date = d.date - interval '1 day' )
+select*
+from customer_growth
+where date = date '2024-04-18' ;
+
+with first_purchase as (
+select customer_name , min(date(date)) as first_purchase_date
+from retail_transactions
+group by 1 )
+, customer_type as ( select date(r.date) as date , r.customer_name , 
+case when date(r.date) = f.first_purchase_date then 'New' else 'Returning'
+end as customer_type
+from retail_transactions as r
+join first_purchase as f
+on r.customer_name = f.customer_name )
+,customer_type_2 as ( select date , customer_type , count(distinct customer_name) as total_customer
+from customer_type
+group by  1,2 )
+, final_query as (
+select dct.date , dct.customer_type , dct.total_customer,
+case when yct.total_customer = 0 then null
+else ( dct.total_customer - yct.total_customer ) *1.0 / yct.total_customer *100 end as dod_growth_type
+from customer_type_2 as dct
+join customer_type_2 as yct
+on yct.date = dct.date - interval '1 day' and dct.customer_type = yct.customer_type )
+select*
+from final_query
+where date = date '2024-04-18'
+
+with first_purchase as (
+select customer_name , min(date(date)) as first_purchase_date
+from retail_transactions
+group by 1 )
+, customer_type as ( select date(r.date) as date , r.customer_name , 
+case when date(r.date) = f.first_purchase_date then 'New' else 'Returning'
+end as customer_type
+from retail_transactions as r
+join first_purchase as f
+on r.customer_name = f.customer_name )
+,customer_type_2 as ( select date , customer_type , count(distinct customer_name) as total_customer
+from customer_type
+group by  1,2 )
+, daily_aov_by_type as (
+select date(rt.date) as date , ct.customer_type , sum(rt.total_cost) / count(distinct rt.transaction_id) as aov
+from retail_transactions as rt
+join customer_type as ct
+on rt.customer_name = ct.customer_name and date(rt.date) = ct.date
+group by 1 ,2 )
+, final_query as ( select d.date , d.customer_type , d.aov ,
+round ( case when y.aov = 0 then null
+else ( d.aov - y.aov ) *1.0 / y.aov *100 end , 2 ) as type_growth_aov
+from daily_aov_by_type as d
+join daily_aov_by_type as y
+on y.date = d.date - interval '1 day' and d.customer_type = y.customer_type )
 select *
-from city_summary
+from final_query
 where date = date '2024-04-18'
